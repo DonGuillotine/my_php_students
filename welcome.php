ec7b1a083deae6dd5f7563b2e3ce8bbd44851223
DonGuillotine <infect3dlab@gmail.com>
2023-02-01 12:12:54 +0100

    PHP ForEach Loop

--- welcome.php
+++ welcome.php
@@ -9,10 +9,20 @@
 <body>
     <!-- PHP Syntax -->
 <?php
-    // PHP FOR LOOP
-    for($age = 0; $age <= 10; $age++){
-        echo "The number is: $age <br>";
-    }
+    // PHP FOREACH LOOP
+  $colors = array("red", "green", "blue", "yellow");
+
+  foreach($colors as $value){
+    echo "$value <br>";
+  }
+  echo '<br>';
+  
+  // Outputing both keys and values of the student array
+  $students = array("Oscar"=>"Football", "Kike"=>"Music");
+
+  foreach($students as $x => $value){
+    echo "$x = $value <br>";
+  }
 ?>
 
 </body>
