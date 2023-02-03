852afc7d90320ab1955482a6d2789ea84d6b9dc3
DonGuillotine <infect3dlab@gmail.com>
2023-02-03 12:11:23 +0100

    Exploring PHP arrays

--- welcome.php
+++ welcome.php
@@ -1,65 +1,43 @@
 <?php 
 declare(strict_types=1);
-// PHP FUNCTIONS
+    // PHP Arrays
+    // Indexed Array
+    $cars = array("Mercedes", "Volkswagen", "Honda");
+    $cars_count = count($cars);
+    echo $cars_count;
+    echo "This is " . $cars[0] . ", " . $cars[1] . ", " . $cars[2] . "<br>";
 
-function writeMsg(){
-    echo "Hello World <br>";
-}
+    for($i = 0; $i < $cars_count; $i++){
+        echo $cars[$i];
+        echo "<br>";
+    }
 
-writeMsg();
+    // Associative Arrays
+    // Second way
+    $age["Oscar"] = "32";
+    $age["Ken"] = "20";
+    $age["Donald"] = "12";
 
-function familyName($first_name, $year){
-    echo "$first_name Okoye was born in $year <br>";
-}
+    // First way
+    $age = array("Oscar"=>"32", "Ken"=>"20", "Donald"=>"12");
+    echo "Oscar is " . $age['Oscar'] . " years old. <br>";
 
-familyName("Janai", 2004);
-familyName("Jarel", 1999);
-familyName("Jonadab", 1982);
-familyName("Jesse", 1716);
-familyName("John", 2003);
+    // Looping through an associative array
 
-// Strict is not enabled
-function addNumbers(int $a, int $b){
-    return $a + $b;
-}
+    foreach($age as $elder => $x_value){
+        echo "The Key is " . $elder . ", Value is " . $x_value;
+        echo "<br>";
+    }
 
-echo addNumbers(5, 6);
-
-//  Default Function Argument
-
-function setHeight(int $minHeight = 50){
-    echo " The Height is : $minHeight<br>";
-}
-
-
-setHeight(340);
-setHeight();
-setHeight(127);
-
-
-// To let a function return a value, use the return statement
-function sum(int $x, int $y){
-    $z = $x + $y;
-    return $z;
-}
-
-echo "5 + 10= " .sum(5, 10) . "<br>";
-echo "7 + 13= " .sum(7, 13) . "<br>";
-echo "2 + 4= " .sum(2, 4) . "<br>";
-
-// Declaring a type for the function return
-function concatNumbers(float $a, float $b) : float{
-    return (int)($a + $b);
-}
-
-echo concatNumbers(1.2, 5.6) . "<br>";
-
-// Passing Arguments by Reference
-function add_five(&$value){
-    $value += 5;
-}
-
-$num = 2;
-add_five($num);
-echo $num;
+    // A multidimensional array is an array containing one or more arrays
+    $cars = array(
+        array("Volove", 22, 18),
+        array("MBW", 15, 13),
+        array("Toyota", 5, 2),
+        array("Honda", 17, 15),
+    );
+    echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+    echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+    echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+    echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
 ?>
